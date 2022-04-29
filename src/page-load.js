1c89daa30d8d6ca893c3944122be3601bb634215
thennek0 <kennethbecaro0@gmail.com>
2022-04-29 18:57:49 +0800

    added navbar layout

--- src/page-load.js
+++ src/page-load.js
@@ -1,10 +1,41 @@
 import './style.css';
+
+function navBar(content) {
+    const navBar = document.createElement("nav");
+    navBar.classList.add("nav-bar");
+
+    const pageTitle = document.createElement("h1");
+    pageTitle.classList.add("page-title");
+    pageTitle.textContent = "sussy restaurant"
+
+
+
+    const navList = document.createElement("ul");
+    const pageOptions = ["Menu","Categories","About","Reviews","Restaurants"];
+    for (let i = 0; i < 5; i++) {
+        const list = document.createElement("li");
+        list.textContent = pageOptions[i];
+        list.classList.add("list");
+        navList.appendChild(list);
+    }
+
+
+    navBar.appendChild(pageTitle);
+    navBar.appendChild(navList);
+
+
+    content.appendChild(navBar);
+}
+function mainContent(content) {
+    const main = document.createElement("main");
+    content.appendChild(main);
+}
+
+
+
 export default function loadPage() {
     const content = document.querySelector("#content");
-    const navBar = document.createElement("nav");
-    const main = document.createElement("main");
-    content.textContent = "bruh";
-    content.classList.add("test");
-    content.appendChild(navBar);
-    content.appendChild(main);
+    navBar(content);
+    mainContent(content);
+
 }
