9a416c470f68cb40ae51002d7c96968b30b8276d
thennek0 <kennethbecaro0@gmail.com>
2022-06-13 12:54:05 +0800

    added feature to remove main content children

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import * as pageLoad from './page-load.js'
 
 pageLoad.loadPage();
-const test = pageLoad.navBar.navLinks;
-test.forEach((element)=>{
+const navLinks = pageLoad.navBar.navLinks;
+const mainContent = document.querySelector("main");
+navLinks.forEach((element)=>{
     element.addEventListener('click',()=>{
         console.log(element.textContent);
+        while (mainContent.firstChild) {
+            mainContent.removeChild(mainContent.firstChild)
+        }
     });
 });
