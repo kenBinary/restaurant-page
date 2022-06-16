b357bfd6be9d77531ba5303e9db90efaa63887e9
thennek0 <kennethbecaro0@gmail.com>
2022-06-16 13:15:02 +0800

    added menu item list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import * as pageLoad from './page-load.js'
-import loadMenu from './menu-page.js'
+import * as menuTab from './menu-page.js'
 
 pageLoad.loadPage();
 const navLinks = pageLoad.navBar.navLinks;
 const mainContent = document.querySelector("main");
-navLinks.forEach((element)=>{
-    element.addEventListener('click',()=>{
-        console.log(element.textContent);
-        while (mainContent.firstChild) {
-            mainContent.removeChild(mainContent.firstChild)
-        }
-        if (element.textContent === "Menu") {
-            loadMenu()
-        }
-    });
-});
+while (mainContent.firstChild) {
+    mainContent.removeChild(mainContent.firstChild)
+}
+menuTab.loadMenu();
+
+// navLinks.forEach((element)=>{
+//     element.addEventListener('click',()=>{
+//         console.log(element.textContent);
+//         while (mainContent.firstChild) {
+//             mainContent.removeChild(mainContent.firstChild)
+//         }
+//         if (element.textContent === "Menu") {
+//             loadMenu()
+//         }
+//     });
+// });
--- src/menu-page.js
+++ src/menu-page.js
@@ -1,12 +1,27 @@
 import './menu-style.css';
 
-function mainContent(mainParent) {
-    const test = document.createElement("div");
-    test.textContent = "hello darkness my old friend";
-    mainParent.appendChild(test);
-    
-}
-export default function loadMenu() {
+const menuContent = (()=>{
+    const firstSection = document.createElement('section');
+    const secondSection = document.createElement('section');
+    firstSection.classList.add("menu-first-section");
+    secondSection.classList.add("menu-second-section");
+    const menuListNames = ["Humba", "Kaldereta", "Sisig","Lechno", "Lumpia"];
+    const menuItems = [];
+    for (let index = 0; index < menuListNames.length; index++) {
+        const newDiv = document.createElement('div');
+        newDiv.textContent = menuListNames[index];
+        menuItems.push(newDiv);
+        firstSection.appendChild(newDiv);
+    }
+    function appendMenu(mainParent) {
+        mainParent.appendChild(firstSection);
+        mainParent.appendChild(secondSection);
+    }
+    return {appendMenu}
+})();
+
+function loadMenu() {
     const content = document.querySelector("main");
-    mainContent(content);
+    menuContent.appendMenu(content);
 }
+export {loadMenu}
